29ce2e35d2d4c082c06f9dcafead09ffc7864c13
gustavo <gustavo.olmedo.formosa@gmail.com>
2021-11-23 23:49:32 -0300

    add player animation

--- src/game/scenes/MainScene.ts
+++ src/game/scenes/MainScene.ts
@@ -1,4 +1,7 @@
 import Phaser from 'phaser';
+import maleImg from '../assets/male.png';
+import maleAtlasJson from '../assets/male_atlas.json';
+import maleAnimJson from '../assets/male_anim.json';
 
 export class MainScene extends Phaser.Scene {
   player?: Phaser.Physics.Matter.Sprite;
@@ -8,11 +11,13 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    console.log('preload');
+    this.load.atlas('male', maleImg, maleAtlasJson);
+    this.load.animation('male_anim', maleAnimJson);
   }
 
   create() {
-    this.player = this.matter.add.sprite(0, 0, "player");
+    this.player = this.matter.add.sprite(0, 0, "male", 'townsfolk_m_idle_1');
+
     this.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -22,6 +27,7 @@ export class MainScene extends Phaser.Scene {
   }
 
   update() { // 60fps
+    this.player?.anims.play('male_walk', true);
     const speed = 2.5;
     let playerVelocity = new Phaser.Math.Vector2();
     if (this.inputKeys?.left.isDown) {
