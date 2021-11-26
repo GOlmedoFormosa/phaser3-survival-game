a7342b109fbd65ebcd1e26601ba9564261d0d676
gustavo <gustavo.olmedo.formosa@gmail.com>
2021-11-25 22:42:33 -0300

    add resources

--- src/game/scenes/MainScene.ts
+++ src/game/scenes/MainScene.ts
@@ -1,10 +1,13 @@
 import Phaser from 'phaser';
 import Player, { IPlayer, IKeys } from '../actors/Player';
 import tilesImg from '../assets/RPG Nature Tileset.png';
+import resourcesImg from '../assets/resources.png';
 import mapJson from '../assets/map.json';
+import resourcesJson from '../assets/resources_atlas.json';
 
 export class MainScene extends Phaser.Scene {
   player?: Phaser.Physics.Matter.Sprite & IPlayer;
+  map?: Phaser.Tilemaps.Tilemap;
   constructor() {
     super('MainScene');
   }
@@ -13,12 +16,13 @@ export class MainScene extends Phaser.Scene {
     Player.preload(this);
     this.load.image('tiles', tilesImg);
     this.load.tilemapTiledJSON('map', mapJson);
+    this.load.atlas('resources', resourcesImg, resourcesJson);
   }
 
   create() {
-    const map = this.make.tilemap({ key: 'map' });
-    const tileSet = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32);
-    const layer1 = map.createLayer('Tile Layer 1', tileSet, 0, 0);
+    this.map = this.make.tilemap({ key: 'map' });
+    const tileSet = this.map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32);
+    const layer1 = this.map.createLayer('Tile Layer 1', tileSet, 0, 0);
     layer1.setCollisionByProperty({ collides: true });
     this.matter.world.convertTilemapLayer(layer1);
 
@@ -31,6 +35,18 @@ export class MainScene extends Phaser.Scene {
       right: Phaser.Input.Keyboard.KeyCodes.D
     }) as IKeys;
 
+    this.addResources();
+
+  }
+
+  addResources() {
+    const resources = this.map?.getObjectLayer('Resources');
+    resources?.objects.forEach(resource => {
+      console.log('res', resource)
+      const resourceItem = new Phaser.Physics.Matter.Sprite(this.matter.world, resource.x || 0, resource.y || 0, 'resources', resource.type);
+      resourceItem.setStatic(true);
+      this.add.existing(resourceItem);
+    })
   }
 
   update() {
